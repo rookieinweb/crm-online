9ca1c438056b60ebda0ad807a6298c0fc83b7ee2
13052927587 <1043677586@qq.com>
2026-08-14 11:59:30 +0800

    迭代开发

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
-import path from 'path'
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
+import path from "path";
 
 export default defineConfig({
   plugins: [uni()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
-})
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
